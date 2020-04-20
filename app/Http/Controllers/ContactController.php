9eb3b46fd16863d2188952f6d463f87023ef5d32
ichlas16 <ichlaskeren@gmail.com>
2020-04-20 07:20:12 +0700

    asdasd

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -80,10 +80,10 @@ class ContactController extends Controller
         }
 
         $friend = $request->get('friend_id');
-        $debug = DB::table('messages')
-        ->where('from_id', $friend)->where('to_id', Auth::id())
-        ->orWhere('from_id', Auth::id())->where('to_id', $friend)
-        ->get()->toArray();
+        // $debug = DB::table('messages')
+        // ->where('from_id', $friend)->where('to_id', Auth::id())
+        // ->orWhere('from_id', Auth::id())->where('to_id', $friend)
+        // ->get()->toArray();
         $test = Contact::all()
         ->where('friends', $friend)->where('host', Auth::id())
         ->toArray();
@@ -99,7 +99,6 @@ class ContactController extends Controller
         ->where('host', $friend)->where('friends', Auth::id())
         ->delete();
 
-        if ($debug){
         DB::table('messaages')
         ->where('from_id', $friend)->where('to_id', Auth::id())
         ->delete();
@@ -107,7 +106,6 @@ class ContactController extends Controller
         DB::table('messages')
         ->where('from_id', Auth::id())->where('to_id', $friend)
         ->delete();
-        }
 
         return response(['msg' => 'contact deleted'], 200);
     }
