69c5fa16ccd721cc21da445f64d94982634b1561
ichlas16 <ichlaskeren@gmail.com>
2020-06-18 09:35:52 +0700

    asdasd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,9 +82,22 @@
                     API Mbah Ichlas
                 </div>
 
-                <div class="links">
+                <div class="postman-run-button" data-postman-action="collection/import"
+                    data-postman-var-1="285469e5f4f5d9b2d590"></div>
+                <script type="text/javascript">
+                    (function (p, o, s, t, m, a, n) {
+                        !p[s] && (p[s] = function () {
+                            (p[t] || (p[t] = [])).push(arguments);
+                        });
+                        !o.getElementById(s + t) && o.getElementsByTagName("head")[0].appendChild((
+                            (n = o.createElement("script")),
+                            (n.id = s + t), (n.async = 1), (n.src = m), n
+                        ));
+                    }(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"));
+                </script>
+                {{-- <div class="links">
                     <a href="https://docs.google.com/document/d/1xqG-VvsIldaNvOWWQoLbl7MgJPLBWZRRb5o-IAHNZ4k/edit?usp=sharing">ANDA FRONT-END? BACA KETENTUAN RESTFUL APINYA DISINI</a>
-                </div>
+                </div> --}}
             </div>
         </div>
     </body>
